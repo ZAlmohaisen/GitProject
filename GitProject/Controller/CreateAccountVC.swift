11fdf6a88eca4a93e9cb7db243c51e23516ab04b
Ziyad almohisen <ziyadalmohisen@Ziyads-MacBook-Pro.local>
2019-04-02 22:04:16 +0300

    UP5

--- GitProject/Controller/CreateAccountVC.swift
+++ GitProject/Controller/CreateAccountVC.swift
@@ -1,5 +1,5 @@
 //
-//  LoginVC.swift
+//  CreateAccountVC.swift
 //  GitProject
 //
 //  Created by Ziyad almohisen on 02/04/2019.
@@ -8,21 +8,18 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class CreateAccountVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        
     }
     
 
     @IBAction func closePressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
-    @IBAction func createAccountBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
-    }
-    
+
 }
